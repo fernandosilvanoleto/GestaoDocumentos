0ebf1b9f560e340c95e81ceed15544d0db080741
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2023-10-22 19:13:00 -0300

    Add Repository Emprestimos

---
+++ Repository/Interfaces/IEmprestimoRepository.cs
@@ -0,0 +1,22 @@
+﻿using GestaoDocumentos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoDocumentos.Repository.Interfaces
+{
+    public interface IEmprestimoRepository
+    {
+        List<EmprestimoModel> BuscarTodosEmprestimos();
+        List<EmprestimoModel> BuscarTodosEmprestimosAtivos();
+        EmprestimoModel ListaPorIdEmprestimo(int idEmprestimo);
+        EmprestimoModel ListaPorIdClientesEmprestimos(int idCliente);
+        EmprestimoModel ListaPorIdBibliotecarioEmprestimos(int IdBibliotecario);
+        EmprestimoModel EditarEmprestimo(EmprestimoModel emprestimo);
+        EmprestimoModel AdicionarEmprestimo(EmprestimoModel emprestimo);
+        bool AjustarStatusEmprestimo(int idEmprestimo, int opcaoStatus);
+        bool AjustarStatusAtivo(int idEmprestimo, int opcaoStatus);
+
+    }
+}
---
+++ Repository/Repositories/EmprestimoRepository.cs
@@ -0,0 +1,57 @@
+﻿using GestaoDocumentos.Models;
+using GestaoDocumentos.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoDocumentos.Repository.Repositories
+{
+    public class EmprestimoRepository : IEmprestimoRepository
+    {
+        public EmprestimoModel AdicionarEmprestimo(EmprestimoModel emprestimo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AjustarStatusAtivo(int idEmprestimo, int opcaoStatus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AjustarStatusEmprestimo(int idEmprestimo, int opcaoStatus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EmprestimoModel> BuscarTodosEmprestimos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EmprestimoModel> BuscarTodosEmprestimosAtivos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmprestimoModel EditarEmprestimo(EmprestimoModel emprestimo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmprestimoModel ListaPorIdBibliotecarioEmprestimos(int IdBibliotecario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmprestimoModel ListaPorIdClientesEmprestimos(int idCliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmprestimoModel ListaPorIdEmprestimo(int idEmprestimo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
