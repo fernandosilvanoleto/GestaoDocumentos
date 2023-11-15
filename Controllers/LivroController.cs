903dd098a1230a404ab562b507a2916ece54b273
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2023-11-15 19:36:12 -0300

    Criação do Controller do Livro

---
+++ Controllers/LivroController.cs
@@ -0,0 +1,32 @@
+﻿using GestaoDocumentos.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoDocumentos.Controllers
+{
+    public class LivroController : Controller
+    {
+        private readonly ILivroRepository _livroRepository;
+        public LivroController(ILivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var livros = _livroRepository.BuscarTodosLivrosAtivos();
+
+            return View(livros);
+        }
+
+        public IActionResult IndexTodosLivros()
+        {
+            var livros = _livroRepository.BuscarTodosLivros();
+
+            return View(livros);
+        }
+    }
+}
