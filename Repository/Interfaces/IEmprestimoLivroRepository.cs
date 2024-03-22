4a5c5253a69245b91e40d41e7484176221dcbbf8
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2024-03-21 21:55:52 -0300

    Adicionar interface e repositório de Emprestimo Livro

---
+++ Repository/Interfaces/IEmprestimoLivroRepository.cs
@@ -0,0 +1,21 @@
+﻿using GestaoDocumentos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoDocumentos.Repository.Interfaces
+{
+    public interface IEmprestimoLivroRepository
+    {
+        List<EmprestimoLivroModel> BuscarTodosEmprestimosLivro();
+        List<EmprestimoLivroModel> BuscarTodosEmprestimosLivroAtivos();
+        EmprestimoLivroModel ListaPorIdEmprestimoLivro(int idEmprestimoLivro);
+        List<EmprestimoLivroModel> ListaPorIdLivro(int idLivro);
+        List<EmprestimoLivroModel> ListaPorIdEmprestimo(int idEmprestimo);
+        EmprestimoLivroModel EditarEmprestimoLivro(EmprestimoLivroModel emprestimoLivro);
+        EmprestimoLivroModel AdicionarEmprestimoLivro(EmprestimoLivroModel emprestimoLivro);
+        bool AjustarStatusEmprestimoLivro(int idEmprestimoLivro, int opcaoStatus);
+        bool AjustarStatusAtivo(int idEmprestimo, int opcaoStatus);
+    }
+}
---
+++ Repository/Repositories/EmprestimoLivroRepository.cs
@@ -0,0 +1,64 @@
+﻿using GestaoDocumentos.Data;
+using GestaoDocumentos.Models;
+using GestaoDocumentos.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoDocumentos.Repository.Repositories
+{
+    public class EmprestimoLivroRepository : IEmprestimoLivroRepository
+    {
+        private readonly BancoContext _bancoContext;
+
+        public EmprestimoLivroRepository(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+        }
+        public EmprestimoLivroModel AdicionarEmprestimoLivro(EmprestimoLivroModel emprestimoLivro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AjustarStatusAtivo(int idEmprestimo, int opcaoStatus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AjustarStatusEmprestimoLivro(int idEmprestimoLivro, int opcaoStatus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EmprestimoLivroModel> BuscarTodosEmprestimosLivro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EmprestimoLivroModel> BuscarTodosEmprestimosLivroAtivos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmprestimoLivroModel EditarEmprestimoLivro(EmprestimoLivroModel emprestimoLivro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EmprestimoLivroModel> ListaPorIdEmprestimo(int idEmprestimo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmprestimoLivroModel ListaPorIdEmprestimoLivro(int idEmprestimoLivro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EmprestimoLivroModel> ListaPorIdLivro(int idLivro)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
